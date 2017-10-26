d4d7c9c931a61856a579ee1e152becaa572751e2
RubioN <33116595+RubioN@users.noreply.github.com>
2017-10-26 16:15:34 +0200

    Add files via upload

--- main.php
+++ main.php
@@ -28,7 +28,7 @@
     </div>
     <div class="collapse navbar-collapse" id="navbar-collapse-main">
       <ul class="nav navbar-nav navbar-right">
-		<li><a href="#home">Accueil</a></li>
+		<li><a href="#">Accueil</a></li>
         <li><a href="#team">L'équipe</a></li>
         <li><a href="#member">Les membres</a></li>
         <li><a href="#action">Les prestations</a></li>
@@ -37,3 +37,73 @@
     </div>
   </div>
 </nav>
+
+<div id="home" class="home_part">
+  <div class="edition_text">
+    <h1>La fine équipe</h1>
+    <a href="#team" class="btn btn-default btn-lg" style="margin-top: 30px;">Poursuivre la visite</a>
+  </div>
+</div>
+
+<div id="team" class="team_part">
+	<div class="title_ann">
+		<span>L'équipe</span>
+	</div>
+	<div class="team_text">
+		<span>Nous sommes des étudiants en informatique à</span>
+	</div>
+	<div style="text-align: center;">
+	<image src="images/epitech.png" style="width:300px; margin: auto;">
+	</div>
+	<div class="team_text">
+		<span>Nous travaillons ensemble dans le même but.</span>
+	</div>
+</div>
+
+<div id="member" class="member_part">
+	<div class="title_ann">
+		<span>Les Membres</span>
+	</div>
+	<div class="col-sm-4">
+		<div class="name_dev">
+			<span>Thomas GEY</span>
+		</div>
+		<div style="text-align: center;">
+		<image src="images/thomas.png" class="img-circle img-responsive" style="width:200px; margin: auto;">
+		</div>
+		<div class="team_text">
+			<i>Description à venir</i>
+		</div>
+	</div>
+		<div class="col-sm-4">
+		<div class="name_dev">
+			<span>Thomas GEY</span>
+		</div>
+		<div style="text-align: center;">
+		<image src="images/thomas.png" class="img-circle img-responsive" style="width:200px; margin: auto;">
+		</div>
+		<div class="team_text">
+			<i>Description à venir</i>
+		</div>
+	</div>
+	<div class="col-sm-4">
+		<div class="name_dev">
+			<span>Thomas GEY</span>
+		</div>
+		<div style="text-align: center;">
+		<image src="images/thomas.png" class="img-circle img-responsive" style="width:200px; margin: auto;">
+		</div>
+		<div class="team_text">
+			<i>Description à venir</i>
+		</div>
+	</div>	
+</div>
+
+<div id="action" class="action_part">
+	<div class="title_ann">
+		<span>Les prestations</span>
+	</div>
+	<div class="team_text">
+		<span>Windows/Web/Mobile</span>
+	</div>
+</div>
