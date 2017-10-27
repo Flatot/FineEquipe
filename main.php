e946ac65273b2dae5873c8f0dcf3ded48b8f4be9
RubioN <33116595+RubioN@users.noreply.github.com>
2017-10-27 10:51:35 +0200

    Add files via upload

--- main.php
+++ main.php
@@ -6,11 +6,13 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
   <link href="css/bootstrap.min.css" rel="stylesheet">
   <link href="css/style.css" rel="stylesheet" />
+  <link href="css/contact.css" rel="stylesheet" />  
 </head>
 <body>
   <script src="js/jquery-1.10.2.js"></script>
   <script src="js/bootstrap.min.js"></script>
   <script src="//maps.google.com/maps/api/js?sensor=true"></script>
+  <script src="js/anime.min.js" type="text/javascript"></script>
   <script src="js/main.js"></script>
 </body>
 </html>
@@ -56,11 +58,11 @@
 	<image src="images/epitech.png" style="width:300px; margin: auto;">
 	</div>
 	<div class="team_text">
-		<span>Nous travaillons ensemble dans le même but.</span>
+		<span>Nous travaillons ensemble dans le même but...</span>
 	</div>
 </div>
 
-<div id="member" class="member_part">
+<div id="member" class="col-sm-12 col-lg-12 col-md-12 member_part">
 	<div class="title_ann">
 		<span>Les Membres</span>
 	</div>
@@ -74,32 +76,77 @@
 		<div class="team_text">
 			<i>Description à venir</i>
 		</div>
-	</div>
-		<div class="col-sm-4">
-		<div class="name_dev">
-			<span>Thomas GEY</span>
-		</div>
-		<div style="text-align: center;">
-		<image src="images/thomas.png" class="img-circle img-responsive" style="width:200px; margin: auto;">
-		</div>
-		<div class="team_text">
-			<i>Description à venir</i>
+		<div class="graph_t">
+			<div id="graph_t"> 
+				<svg width="140" height="140" viewBox="0 0 140 140">
+				  <g fill="none" fill-rule="evenodd">
+					<g fill="#ff3333" fill-opacity=".15" transform="translate(0 6)">
+					  <polygon points="70 0 136.574 48.369 111.145 126.631 28.855 126.631 3.426 48.369"></polygon>
+					  <polygon points="70 18 119.455 53.931 100.565 112.069 39.435 112.069 20.545 53.931"></polygon>
+					  <polygon points="70 34.86 101.727 57.911 89.609 95.209 50.391 95.209 38.273 57.911"></polygon>
+					  <polygon points="70 50.898 84.864 61.697 79.186 79.171 60.814 79.171 55.136 61.697"></polygon>
+					</g>
+					<polygon class="graph_t" stroke-width="3" stroke="#ff3333" points="70 33.84167800036825 118.99507776468423 59.79007776468422 106.51314458847357 126.31483352973669 56.64422235315779 91.78932199963175 13.267678000368248 57.84253341189468 "></polygon>
+				  </g>
+				</svg>
+			</div>
 		</div>
 	</div>
 	<div class="col-sm-4">
 		<div class="name_dev">
-			<span>Thomas GEY</span>
+			<span>Nicolas RUBIO</span>
 		</div>
 		<div style="text-align: center;">
-		<image src="images/thomas.png" class="img-circle img-responsive" style="width:200px; margin: auto;">
+		<image src="images/nicolas.png" class="img-circle img-responsive" style="width:200px; margin: auto;">
 		</div>
 		<div class="team_text">
 			<i>Description à venir</i>
 		</div>
+		<div class="graph_n">
+			<div id="graph_n"> 
+				<svg width="140" height="140" viewBox="0 0 140 140">
+				  <g fill="none" fill-rule="evenodd">
+					<g fill="#00b300" fill-opacity=".15" transform="translate(0 6)">
+					  <polygon points="70 0 136.574 48.369 111.145 126.631 28.855 126.631 3.426 48.369"></polygon>
+					  <polygon points="70 18 119.455 53.931 100.565 112.069 39.435 112.069 20.545 53.931"></polygon>
+					  <polygon points="70 34.86 101.727 57.911 89.609 95.209 50.391 95.209 38.273 57.911"></polygon>
+					  <polygon points="70 50.898 84.864 61.697 79.186 79.171 60.814 79.171 55.136 61.697"></polygon>
+					</g>
+					<polygon class="graph_n" stroke-width="3" stroke="#00b300" points="70 33.84167800036825 118.99507776468423 59.79007776468422 106.51314458847357 126.31483352973669 56.64422235315779 91.78932199963175 13.267678000368248 57.84253341189468 "></polygon>
+				  </g>
+				</svg>
+			</div>
+		</div>
+	</div>
+	<div class="col-sm-4">
+		<div class="name_dev">
+			<span>Tristan FLATOT</span>
+		</div>
+		<div style="text-align: center;">
+		<image src="images/tristan.jpg" class="img-circle img-responsive" style="width:200px; margin: auto;">
+		</div>
+		<div class="team_text">
+			<i>Description à venir</i>
+		</div>
+		<div class="graph_tr">
+			<div id="graph_tr"> 
+				<svg width="140" height="140" viewBox="0 0 140 140">
+				  <g fill="none" fill-rule="evenodd">
+					<g fill="#4d4dff" fill-opacity=".15" transform="translate(0 6)">
+					  <polygon points="70 0 136.574 48.369 111.145 126.631 28.855 126.631 3.426 48.369"></polygon>
+					  <polygon points="70 18 119.455 53.931 100.565 112.069 39.435 112.069 20.545 53.931"></polygon>
+					  <polygon points="70 34.86 101.727 57.911 89.609 95.209 50.391 95.209 38.273 57.911"></polygon>
+					  <polygon points="70 50.898 84.864 61.697 79.186 79.171 60.814 79.171 55.136 61.697"></polygon>
+					</g>
+					<polygon class="graph_tr" stroke-width="3" stroke="#4d4dff" points="70 33.84167800036825 118.99507776468423 59.79007776468422 106.51314458847357 126.31483352973669 56.64422235315779 91.78932199963175 13.267678000368248 57.84253341189468 "></polygon>
+				  </g>
+				</svg>
+			</div>
+		</div>
 	</div>	
 </div>
 
-<div id="action" class="action_part">
+<div id="action" class="col-sm-12 col-lg-12 col-md-12 action_part">
 	<div class="title_ann">
 		<span>Les prestations</span>
 	</div>
@@ -107,3 +154,63 @@
 		<span>Windows/Web/Mobile</span>
 	</div>
 </div>
+
+ <section id="contact">
+      <div class="container">
+        <div class="row">
+          <div class="col-xs-12">
+            <form method="post" action="send_mail.php" name="sentEmail" id="sentEmail">
+              <div class="rst-group-title">
+                <h4>Contactez-nous</h4>
+              </div>
+              <div class="row">
+                <div class="col-sm-6">
+                  <div class="form-group">
+                    <input class="rst-page-input" type="text" placeholder="Nom" name="nom" id="nom"/>
+                  </div>
+                  <div class="form-group">
+                    <input class="rst-page-input" type="text" placeholder="Email" name="email" id="email"/>
+                  </div>
+                  <div class="form-group">
+                    <input class="rst-page-input" type="text" placeholder="Sujet" name="subject" id="subject"/>
+                  </div>
+                </div>
+                <div class="col-sm-6">
+                  <div class="form-group">
+                    <textarea class="rst-page-input" name="message" id="message" cols="30" rows="10" placeholder="Message"></textarea>
+                  </div>
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-xs-12 rst-contactsubmit">
+                  <input class="btn btn-primary btn-lg" type="submit" value="Envoyer"/>
+                </div>
+              </div>
+              </form>
+          </div>
+        </div>
+      </div>
+      <!-- <div class="rst-contact-map">
+        <section id="cd-google-map">
+          <div id="google-container"></div>
+          <div class="rst-map-container">
+            <div class="rst-contact-info">
+              <h4 class="rst-page-title">La Fine Equipe</h4>
+              <p></p>
+              <ul class="ul_none">
+                <li><a target="_blank" href="https://goo.gl/maps/sWk4fAGepwq"> 69 007 Lyon - France</a></li>
+                <li><a href="mailto:fine.equipe@gmail.com">fine.equipe@gmail.com</a></li>
+              </ul>
+            </div>
+          </div>
+        </section>
+      </div>
+          </section> -->
+    <!--- End Content -->
+  
+  <script src="js/jquery.1.11.1.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCgGyzOzpWh_mTpdx-UPt92W6GI8hE7P3M"></script>
+  <script src="js/contact.js"></script>
+</body>
+</html>
