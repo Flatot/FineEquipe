f965feebd49f03a152eab93cbf05d511a86a95fa
RubioN <33116595+RubioN@users.noreply.github.com>
2017-11-02 14:04:58 +0100

    Add files via upload

--- main.php
+++ main.php
@@ -1,7 +1,7 @@
 <html lang="en" >
 <head>
   <meta charset="utf-8" />
-  <meta name="author" content="Script Tutorials" />
+  <meta name="La fine equipe" content="Web site" />
   <title>La Fine Equipe</title>
   <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
   <link href="css/bootstrap.min.css" rel="stylesheet">
